d5521b238a768b741932540c703c69b7f8edd201
Ian Anderson <iangeorgeanderson@gmail.com>
2019-04-27 21:38:25 -0400

    comment section updating
    Comment section is now updating live with the api.  Could still use some cleaning up and streamlining.
    
    Timestamp needs to be converted into mm/dd/yyyy still

--- Sprint-3/Scripts/index.js
+++ Sprint-3/Scripts/index.js
@@ -13,7 +13,6 @@ promise.catch(error => {
     console.log(error)
 })
 
-
 var commentForm = document.getElementById('commentForm');
 
 var comment1 = {
@@ -32,100 +31,107 @@ var comment3 = {
     comment: 'How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!'
 };
 
-//console.log(comment1.comment);
-//console.log(comment2.comment);
-//console.log(comment3.comment);
-
 var commentArray = [];
 
 commentArray.push(comment1);
 commentArray.push(comment2);
 commentArray.push(comment3);
 
-//console.log(commentArray);
-
 document.addEventListener('submit', function() {
     event.preventDefault();
 
-    var date = new Date()
-    var calendar = (date.getMonth() + 1)  + "/" + date.getDate() + "/" + date.getFullYear()
-   
-    var newComment = {
-        name: event.target.commName.value,
-        date: calendar,
+    axios.post("https://project-1-api.herokuapp.com/comments/?api_key=5d3f6448-e9d0-43f9-b9ba-53ff05ab10c0", {
+        name:event.target.commName.value,
         comment: event.target.commComment.value
-    }
-    commentArray.push(newComment);
-
-    //console.log(commentArray);
-    commentForm.reset();
-    
-    //var parent = document.getElementById("allComments")
-    
-    var elim = document.querySelectorAll(".section-five__comment-container, .section-five__divider");
-    console.log(elim);
-    //parent.remove(elim);
-    elim.forEach(element => {
-        element.remove()
     })
+        .then(postResponse => {
+            axios.get("https://project-1-api.herokuapp.com/comments/?api_key=5d3f6448-e9d0-43f9-b9ba-53ff05ab10c0")
+            .then(newList =>{
+                console.table(newList.data)
+                console.log("this is your updated get request")
 
-    var arrayLength = commentArray.length;
-    for (var i = 0; i < arrayLength; i++) {
-    createComment(commentArray[i])
-    }
-});
+                commentForm.reset();
+        
+                var elim = document.querySelectorAll(".section-five__comment-container, .section-five__divider");
+                console.log(elim);
+                
+                elim.forEach(element => {
+                    element.remove()
+                })
 
-function createComment(newComment){
+                var apiArray = newList.data
+                var arrayLength = apiArray.length;
 
-    const allComments = document.getElementById("allComments");
+                for (var i = 0; i < arrayLength; i++) {
+                createComment(apiArray[i])
+                }
 
-    var commentDivider = document.createElement("div");
-    commentDivider.classList.add("section-five__divider");
+                function createComment(newComment){
 
-    allComments.appendChild(commentDivider);
+                    const allComments = document.getElementById("allComments");
+                
+                    var commentDivider = document.createElement("div");
+                    commentDivider.classList.add("section-five__divider");
+                
+                    allComments.appendChild(commentDivider);
+                
+                    var commentContainer = document.createElement("div");
+                    commentContainer.classList.add("section-five__comment-container");
+                
+                    allComments.appendChild(commentContainer);
+                    
+                    var commentPicture = document.createElement("div");
+                    commentPicture.classList.add("section-five__old-comment-picture");
+                
+                    commentContainer.appendChild(commentPicture);
+                
+                    var comment = document.createElement("div");
+                    comment.classList.add("section-five__old-comment");
+                
+                    commentContainer.appendChild(comment);
+                
+                    var commentCredentials = document.createElement("div");
+                    commentCredentials.classList.add("section-five__old-comment-credentials");
+                
+                    comment.appendChild(commentCredentials);
+                
+                    var commentName = document.createElement("h3");
+                    commentName.classList.add("section-five__old-comment--name");
+                    
+                    //InnerHTML Name field
+                    commentName.innerHTML = apiArray[i].name;
+                
+                    commentCredentials.appendChild(commentName);
+                
+                    var commentDate = document.createElement("h4");
+                    commentDate.classList.add("section-five__old-comment--date");
+                    
+                    //InnerHTML Date field
+                    commentDate.innerHTML = apiArray[i].timestamp;
+                    
+                    commentCredentials.appendChild(commentDate);
+                
+                    var commentText = document.createElement("h4");
+                    commentText.classList.add("section-five__old-comment--comment");
+                    
+                    //InnerHTML Comment field
+                    commentText.innerHTML = apiArray[i].comment;
+                
+                    comment.appendChild(commentText);
+                }
+            })
+        })
+            .catch(error => {
+            console.log(error);
+        });
+});  
 
-    var commentContainer = document.createElement("div");
-    commentContainer.classList.add("section-five__comment-container");
+// var date = new Date()
+// var calendar = (date.getMonth() + 1)  + "/" + date.getDate() + "/" + date.getFullYear()
 
-    allComments.appendChild(commentContainer);
-    
-    var commentPicture = document.createElement("div");
-    commentPicture.classList.add("section-five__old-comment-picture");
-
-    commentContainer.appendChild(commentPicture);
-
-    var comment = document.createElement("div");
-    comment.classList.add("section-five__old-comment");
-
-    commentContainer.appendChild(comment);
-
-    var commentCredentials = document.createElement("div");
-    commentCredentials.classList.add("section-five__old-comment-credentials");
-
-    comment.appendChild(commentCredentials);
-
-    var commentName = document.createElement("h3");
-    commentName.classList.add("section-five__old-comment--name");
-    
-    //InnerHTML Name field
-    commentName.innerHTML = newComment.name;
-
-    commentCredentials.appendChild(commentName);
-
-    var commentDate = document.createElement("h4");
-    commentDate.classList.add("section-five__old-comment--date");
-    
-    //InnerHTML Date field
-    commentDate.innerHTML = newComment.date;
-    
-    commentCredentials.appendChild(commentDate);
-
-    var commentText = document.createElement("h4");
-    commentText.classList.add("section-five__old-comment--comment");
-    
-    //InnerHTML Comment field
-    commentText.innerHTML = newComment.comment;
-
-    comment.appendChild(commentText);
-
-}
+// var newComment = {
+//             name: event.target.commName.value,
+//             date: calendar,
+//             comment: event.target.commComment.value
+//         }
+//         commentArray.push(newComment);
